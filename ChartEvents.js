de51c9bbf4cb455e4c8141ad7d9dd94c1ba60505
darrincecil <darrinthomasascecil@gmail.com>
2016-05-01 15:53:33 -0400

    simple fluent api around html circle.  i like

--- ChartEvents.js
+++ ChartEvents.js
@@ -3,12 +3,6 @@ var ee = require("event-emitter");
 var emitter = ee({});
 var axis = require("./xaxis/axis");
 require("./rect/hide");
-d3.select("svg").append("rect").attr({
-  width:100,
-  height:100,
-  x: 100,
-  y:2
-})
 
 emitter.on('onButtonClick',(time)=>{
   console.log("the time when you click the button",time);
--- circle/hide.js
+++ circle/hide.js
@@ -1,12 +1,65 @@
-var d3 = require("d3");
-var attrs = {
+const d3 = require("d3");
+const _ = require("underscore");
+const Promise = require("bluebird");
+var config = {
   cx: 20,
   cy: 20,
   r: 20
 }
-var circle = d3.select("svg").append("circle").attr(attrs);
+var circle_ = d3.select("svg").append("circle").attr(config);
 
-circle.transition().duration(1000).attrTween("r",function(){
-  var r = +d3.select(this).attr("r");
-  return d3.interpolateNumber(r,0);
-});
+let getConfig = (config,newOpts)=> _.extend(config,newOpts);
+
+window.circle =circle;
+function circle(opts){
+  config = getConfig(config,opts);
+  circle.radius = (r)=>{
+    circle_.attr("r",r);
+    return circle;
+  }
+  circle.x = (x)=>{
+    circle_.attr("cx",x);
+    return circle;
+  }
+  circle.cx = circle.x;
+  circle.y = (y)=>{
+    circle_.attr("cy",y);
+    return circle;
+  }
+  circle.cy = circle.y;
+  circle.move = function(x,y){
+    var translate = circle_.attr("transform") || [0,0];
+    circle_.attr("transform",function(){
+      var xVal = translate[0] + x;
+      var yVal = translate[1] + y;
+      return `translate(${xVal},${yVal})`;
+
+    });
+    //return circle;
+
+  }
+  circle.animate = (prop,val)=>{
+    var start = 0;
+    return new Promise((resolve)=>{
+      circle_.transition()
+        .duration(2000)
+        .each('start',function(){
+          start++;
+        })
+        .attrTween(prop,function(){
+          var start = d3.select(this).attr(prop);
+          return d3.interpolateNumber(start,val);
+        })
+        .each("end",function(){
+          start--;
+          if(start===0){
+            resolve(circle);
+          }
+        })
+      return circle;
+    });
+
+  }
+  return circle;
+}
+circle();
