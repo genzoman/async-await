f1bef569b059fd93850d41021d527a6dba9687bf
darrincecil <darrinthomasascecil@gmail.com>
2016-05-19 21:18:32 -0400

    path draw in

--- paths/animations/drawIn.js
+++ paths/animations/drawIn.js
@@ -1,12 +1,17 @@
+//drawIn.js
 var d3 = require("d3");
-module.exports = function(path){
-  var totalLength = path.node().getTotalLength();
-  path
-    .attr("stroke-dasharray", totalLength+ " " + totalLength)
-    .attr("stroke-dashoffset", totalLength)
-    .transition()
-      .duration(600)
-      .ease("bounce")
-      .attr("stroke-dashoffset", 0);
-
-}
+var circlePath = require("../../paths/circle-path");
+(function() {
+  'use strict';
+  d3.transition.prototype.drawIn = function(){
+    let elem = this.node();
+    var totalLength = elem.getTotalLength();
+    return d3.select(elem)
+        .attr("stroke-dasharray", totalLength+ " " + totalLength)
+        .attr("stroke-dashoffset", totalLength)
+        .transition()
+          .duration(600)
+          .ease("bounce")
+          .attr("stroke-dashoffset", 0);
+  }
+}());
